f1e9fe0729ea1877a79e23f9b0fb2c87d2c744a0
Eric <anderson.ericandrew@gmail.com>
2023-07-28 23:53:08 -0700

    add skeleton html with no style for submit and view page

--- client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
+++ client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const SubmitHours = () => {
   return (
-    <div>SubmitHours</div>
+    <div className='submitContainer'>
+      <div className="dateContainer">
+        Date: 
+        <br />probably calendar input plugin
+      </div>
+      <div className="timeContainer">
+        Time:
+        <br />time plugin maybe
+      </div>
+      <div className="locationContainer">
+        Location:
+        <br />dropdown with spots
+      </div>
+      <div className="descriptionContainer">
+        Description:
+        <br />Write notes here input
+      </div>
+    </div>
   )
 }
 
--- client/src/Components/Dashboard/DashComponents/ViewHours.jsx
+++ client/src/Components/Dashboard/DashComponents/ViewHours.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ViewHours = () => {
   return (
-    <div>ViewHours</div>
+    <div className='viewContainer'>
+      <div className="dateContainer">
+        Dates:
+        <div className="flex fromToContainer">
+          <div className="fromDate">from:</div>
+          <div className="toDate">to:</div>
+        </div> 
+      </div>
+      <div className="hoursContainer">list out dates and hours here in a table or larger calendar</div>
+    </div>
   )
 }
 
