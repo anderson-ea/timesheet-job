35439d4b7eb2064497afc72f27b6924a4605daca
Eric <anderson.ericandrew@gmail.com>
2023-07-29 18:45:24 -0700

    update style and structure of submit hours page. add date picker

--- client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
+++ client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 const SubmitHours = () => {
+  const [startDate, setStartDate] = useState(new Date())
+
+  const submitHours = () => {
+    console.log(startDate)
+  }
+
   return (
     <div className='submitContainer'>
       <div className="dateContainer">
         Date: 
-        <br />probably calendar input plugin
+        <br /><DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
       </div>
       <div className="timeContainer">
         Time:
@@ -13,13 +21,20 @@ const SubmitHours = () => {
       </div>
       <div className="locationContainer">
         Location:
-        <br />dropdown with spots
+        <br />
+        <select name="locations" id="locations">
+          <option value="Elk Park Rd">Elk Park Rd</option>
+          <option value="Little Gem">Little Gem</option>
+          <option value="Peak View Park">Peak View Park</option>
+          <option value="Residential">Residential</option>
+          <option value="Village at Tamarac">Village at Tamarac</option>
+        </select>
       </div>
       <div className="descriptionContainer">
         Description:
-        <br />Write notes here input
+        <br /><textarea className="descriptionText" type="text"></textarea>
       </div>
-      <button className="btn" type='submit'>Submit</button>
+      <button className="btn" type='submit' onClick={submitHours}>Submit</button>
     </div>
   )
 }
--- client/src/Components/Dashboard/DashComponents/ViewHours.jsx
+++ client/src/Components/Dashboard/DashComponents/ViewHours.jsx
@@ -4,12 +4,12 @@ const ViewHours = () => {
   return (
     <div className='viewContainer'>
       <div className="dateContainer">
-        Dates:
+        Select Dates:
         <div className="flex fromToContainer">
           <div className="fromDate">from:</div>
           <div className="toDate">to: empty input div here with stuff</div>
         </div>
-        <button className="btn" type='submit'>Submit</button> 
+        <button className="btn" type='submit'>View Hours</button> 
       </div>
       <div className="hoursContainer">list out dates and hours here in a table or larger calendar</div>
     </div>
