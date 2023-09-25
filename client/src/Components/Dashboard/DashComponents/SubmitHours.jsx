17f967607ac6bf782b36e75900bc255a31c6da1a
Eric <anderson.ericandrew@gmail.com>
2023-09-25 14:54:34 -0700

    remove unneeded line of code

--- client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
+++ client/src/Components/Dashboard/DashComponents/SubmitHours.jsx
@@ -9,7 +9,6 @@ export const SubmitHours = () => {
   const [description, setDescription] = useState('')
   const [formStatusHolder, setFormStatusHolder] = useState('message')
   const [formStatus, setFormStatus] = useState('')
-  const [formFull, setFormFull] = useState()
 
   const { auth } = useContext(AuthContext)
 
